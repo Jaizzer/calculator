5fed33e0d7f5f93c2e254a7783cdd3661f16addc
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-05-21 09:59:17 +0800

    Add event listener to number buttons for appending numbers to display when pressed

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let mainDisplay = document.querySelector('.main-display');
 
 const numberButtons = Array.from(document.querySelectorAll('button.number'));
+
+numberButtons.forEach(function(numberButton) {
+    numberButton.addEventListener('click', function(event) {
+        let pressedNumber = event.target.textContent;
+        mainDisplay.textContent = mainDisplay.textContent + pressedNumber;
+    });
+})
